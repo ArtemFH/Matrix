ad0c3a58cc7b98f2689e997a47db8f3fc622d7ee
ArtemFH <artembereza1@mail.ru>
2021-10-29 06:44:36 +0700

    Abandoned

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,23 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {HttpClientModule} from '@angular/common/http';
+import {FormsModule} from '@angular/forms';
+import {MatrixComponent} from './matrix/matrix.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MatrixComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
